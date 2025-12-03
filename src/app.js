be65173ba2d32c83d99a3146998c8f8c91b7bfa4
Abhishek Gupta <it@mahatta.com>
2025-12-03 14:12:14 +0530

    cloudndary

--- src/app.js
+++ src/app.js
@@ -12,8 +12,13 @@ import ticketRoutes from './routes/ticket.routes.js';
 import adminRoutes from './routes/admin.routes.js';
 import sellGadgetRoutes from './routes/sell-gadget.routes.js';
 import claimRoutes from './routes/waranty.routes.js';
+import { v2 as cloudinary } from "cloudinary";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
+import dotenv from 'dotenv';
+import multer from "multer";
 
 const app = express();
+dotenv.config();
 
 app.use(cors({
   origin: '*',
@@ -23,6 +28,22 @@ app.use(cors({
 
 app.use(express.json({ limit: '50mb' })); 
 app.options('*', cors());
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const storage = new CloudinaryStorage({
+  cloudinary,
+  params: {
+    folder: "uploads",
+    resource_type: "auto", 
+  },
+});
+
+const upload = multer({ storage });
 
 dbConnect();
 app.use('/api/v1/user', userRoutes);
@@ -37,6 +58,15 @@ app.use('/api/v1/invoice', invoiceRoutes);
 app.use('/api/v1/support', ticketRoutes);
 
 app.use('/api/v1/admin', adminRoutes);
+
+app.post("/uploadFile", upload.single("file"), (req, res) => {
+  res.json({
+    message: "File uploaded successfully!",
+    type: req.file.mimetype,
+    url: req.file.path,
+  });
+});
+
 
 app.use(errorHandler);
 
